e4754c65de2292c5b699223858e080e13cf96db8
Mahathi162 <79361867+Mahathi162@users.noreply.github.com>
2021-02-25 23:12:51 +0530

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,44 +5,28 @@
 # Copyright:: 2021, The Authors, All Rights Reserved.
 include_recipe 'chocolatey::default'
 
-chocolatey_package '7zip' do
-  action :install
-  version '19.0'
-end
+chocolatey_packages = {
+  'javaruntime' => '7.0.71',
+  '7zip' => '19.0',
+  'curl' => '7.68.0',
+  'vscode' => '1.41.1',
+  'windows-sdk-10.1' => '10.1.18362.1',
+  'msbuild.communitytasks' => '1.4.0.74',
+  'python2' => '2.7.17',
+  'git' => '2.25.0',
+}
 
-chocolatey_package 'curl' do
-  action :install
-  version '7.68.0'
-end
-
-chocolatey_package 'vscode' do
-  action :install
-  version '1.41.1'
-end
-
-chocolatey_package 'git' do
-  action :install
-  version '2.25.0'
-end
-
-chocolatey_package 'javaruntime' do
-  action :install
-  version '7.0.71'
-end
-
-chocolatey_package 'python2' do
-  action :install
-  version '2.7.17'
-end
-
-chocolatey_package 'msbuild.communitytasks' do
-  action :install
-  version '1.4.0.74'
+chocolatey_packages.each do |package, package_version|
+  chocolatey_package package do
+    version package_version
+    action :install
+  end
 end
 
 cicd_env_vars = {
   'GIT_HOME' => 'C:\Program Files\Git\\',
   'GIT_SSH_VARIANT' => 'ssh',
+  'FrameworkSDK10' => 'C:\Program Files (x86)\Windows Kits\10\\',
   'MSBuildComTasks' => 'C:\Program Files (x86)\MSBuild\MSBuildCommunityTasks\\',
 }
 
